dfcc28d38b9ad2ee88c2a01eb096a2206d41aa1a
su-myn <134207439+su-myn@users.noreply.github.com>
2025-04-21 16:30:52 +0800

    - add Procfile

--- app.py
+++ app.py
@@ -2319,7 +2319,7 @@ with app.app_context():
 
 #if __name__ == '__main__':
 #    app.run(debug=True)
-    
+
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
